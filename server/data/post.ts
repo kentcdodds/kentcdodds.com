f5f416567989c05c0107c615d35c73e847b49796
Kent C. Dodds <me+github@kentcdodds.com>
2020-12-10 14:50:17 -0700

    make it work now, add github later

--- server/data/post.ts
+++ server/data/post.ts
@@ -7,13 +7,9 @@ import html from 'remark-html'
 import visit from 'unist-util-visit'
 import remarkPrism from 'remark-prism'
 import type {Post} from 'types'
-import config from '../blog.config.json'
 
 async function getPost(name: string): Promise<Post> {
-  const contents =
-    process.env.NODE_ENV === 'production'
-      ? await getPostFromGitHub(name)
-      : await getPostFromFS(name)
+  const contents = await getPostFromFS(name)
 
   const {body, attributes} = parseFrontMatter<Post['attributes']>(contents)
   const result = await remark()
@@ -35,10 +31,7 @@ async function getPost(name: string): Promise<Post> {
 }
 
 async function getPosts(): Promise<Array<Post>> {
-  const files =
-    process.env.NODE_ENV === 'production'
-      ? await getPostsFromGitHub()
-      : await getPostsFromFS()
+  const files = await getPostsFromFS()
 
   return files
     .map(({name, contents}) => {
@@ -71,34 +64,10 @@ async function getPostsFromFS() {
   return posts
 }
 
-// TODO: make this work
-async function getPostsFromGitHub() {
-  const url = `https://api.github.com/repos/${config.repo}/contents/content/blog?ref=${config.branch}`
-  const res = await fetch(url, {
-    headers: {
-      authorization: `token ${process.env.GH_TOKEN}`,
-    },
-  })
-  const files: Array<{name: string; download_url: string}> = await res.json()
-  return Promise.all(
-    files.map(async ({name, download_url}) => {
-      const contentsRes = await fetch(download_url)
-      const contents = await contentsRes.text()
-      return {name, contents}
-    }),
-  )
-}
-
 async function getPostFromFS(param: string) {
   const dir = path.join(__dirname, '../../content/blog')
   const file = await fs.readFile(path.join(dir, `${param}/index.md`))
   return file.toString()
-}
-
-async function getPostFromGitHub(name: string) {
-  const url = `https://raw.githubusercontent.com/${config.repo}/${config.branch}/app/posts/${name}.md`
-  const res = await fetch(url)
-  return res.text()
 }
 
 export {getPost, getPosts}
