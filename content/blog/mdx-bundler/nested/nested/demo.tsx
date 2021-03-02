84776e1144c1cd18f4bd3829822c1ffcce8a37ca
Kent C. Dodds <me+github@kentcdodds.com>
2021-03-01 23:42:40 -0700

    change stuff in the github UI

--- content/blog/mdx-bundler/nested/nested/demo.tsx
+++ content/blog/mdx-bundler/nested/nested/demo.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react'
 
 function NestedNestedDemo({children}: {children: React.ReactElement}) {
-  return <div>Here are your nested nested demo children: {children}</div>
+  return (
+    <>
+      <div>Here are your nested nested demo children: {children}</div>
+      <div>And this was changed in the GitHub UI. Woo</div>
+    </>
+  )
 }
 
 export default NestedNestedDemo
