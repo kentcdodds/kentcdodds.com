eb403126bcb533a9ec2ed3d9468893621e0ed313
Kent C. Dodds <me+github@kentcdodds.com>
2021-03-01 19:54:52 -0700

    add example

---
+++ content/blog/mdx-bundler/nested/nested/demo.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+function NestedNestedDemo({children}: {children: React.ReactElement}) {
+  return <div>Here are your nested nested demo children: {children}</div>
+}
+
+export default NestedNestedDemo
