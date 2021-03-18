6c1013034f15abc3d7fde59ad81c1deee5c736e9
Kent C. Dodds <me+github@kentcdodds.com>
2021-03-18 16:54:32 -0600

    add clubs and auto-optimize all cloudinary images

--- app/utils/compile-mdx.server.ts
+++ app/utils/compile-mdx.server.ts
@@ -1,5 +1,6 @@
 import {bundleMDX} from 'mdx-bundler'
 import visit from 'unist-util-visit'
+import type {PluggableList} from 'unified'
 import remarkPrism from 'remark-prism'
 import remarkEmbedder from '@remark-embedder/core'
 import oembedTransformer from '@remark-embedder/transformer-oembed'
@@ -23,6 +24,9 @@ const getOEmbedConfig: OEmbedConfig = ({provider}) => {
 
 const cache = new Cache()
 
+// yes, I did write this myself 😬
+const cloudinaryUrlRegex = /^https?:\/\/res\.cloudinary\.com\/(?<cloudName>.+?)\/image\/upload(\/(?<transforms>(?!v\d+).+?))?(\/(?<version>v\d+))?\/(?<publicId>.+$)/
+
 async function compileMdx(slug: string, githubFiles: Array<GitHubFile>) {
   const indexRegex = new RegExp(`${slug}\\/index.mdx?$`)
   const indexFile = githubFiles.find(({path}) => indexRegex.test(path))
@@ -40,27 +44,59 @@ async function compileMdx(slug: string, githubFiles: Array<GitHubFile>) {
     valueName: 'content',
   })
 
+  const remarkPlugins: PluggableList = [
+    remarkPrism,
+    function remapImageUrls() {
+      return function transformer(tree: Node) {
+        visit(tree, 'image', function visitor(node) {
+          let url = String(node.url)
+          if (url.startsWith('./')) {
+            url = url.replace('./', '')
+            node.url = `/__img/content/blog/${slug}/${url}`
+          }
+        })
+      }
+    },
+    function optimizeCloudinaryImages() {
+      return function transformer(tree: Node) {
+        visit(tree, 'image', function visitor(node) {
+          const urlString = String(node.url)
+          const match = urlString.match(cloudinaryUrlRegex)
+          const groups = match?.groups
+          if (groups) {
+            const {cloudName, transforms, version, publicId} = groups as {
+              cloudName: string
+              transforms?: string
+              version?: string
+              publicId: string
+            }
+            // don't add transforms if they're already included
+            if (transforms) return
+            const defaultTransforms = 'f_auto,q_auto'
+            node.url = [
+              `https://res.cloudinary.com/${cloudName}/image/upload`,
+              defaultTransforms,
+              version,
+              publicId,
+            ]
+              .filter(Boolean)
+              .join('/')
+          }
+        })
+      }
+    },
+    [
+      remarkEmbedder,
+      {cache, transformers: [[oembedTransformer, getOEmbedConfig]]},
+    ],
+  ]
+
   const {frontmatter, code} = await bundleMDX(indexFile.content, {
     files,
     xdmOptions(input, options) {
       options.remarkPlugins = [
         ...(options.remarkPlugins ?? []),
-        remarkPrism,
-        function remapImageUrls() {
-          return function transformer(tree: Node) {
-            visit(tree, 'image', function visitor(node) {
-              let url = String(node.url)
-              if (url.startsWith('./')) {
-                url = url.replace('./', '')
-                node.url = `/__img/content/blog/${slug}/${url}`
-              }
-            })
-          }
-        },
-        [
-          remarkEmbedder,
-          {cache, transformers: [[oembedTransformer, getOEmbedConfig]]},
-        ],
+        ...remarkPlugins,
       ]
       return options
     },
