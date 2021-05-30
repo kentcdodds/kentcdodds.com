9b4750ec0879521d36023f63bc3275d1c297348d
Kent C. Dodds <me+github@kentcdodds.com>
2021-05-29 18:27:09 -0600

    maybe this will work well

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,7 @@ module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   routes(defineRoutes) {
     return defineRoutes(route => {
-      if (process.env.NODE_ENV !== 'production') {
+      if (process.env.NODE_ENV !== 'fly') {
         route('/__tests/login', '__test_routes__/login.tsx')
       }
     })
