edb9b3e26de38f46f726ed47722f910a8820183e
Kent C. Dodds <me+github@kentcdodds.com>
2021-05-29 22:58:59 -0600

    make test routes opt-in instead

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,7 @@ module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   routes(defineRoutes) {
     return defineRoutes(route => {
-      if (process.env.NODE_ENV !== 'fly') {
+      if (process.env.ENABLE_TEST_ROUTES) {
         route('/__tests/login', '__test_routes__/login.tsx')
       }
     })
