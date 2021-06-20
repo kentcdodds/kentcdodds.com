2feca5bcb1ed8434a015a94b2df0ab049a28f241
Kent C. Dodds <me+github@kentcdodds.com>
2021-06-19 21:39:55 -0600

    chore: make this more specific

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,7 @@ module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   routes(defineRoutes) {
     return defineRoutes(route => {
-      if (process.env.ENABLE_TEST_ROUTES) {
+      if (process.env.ENABLE_TEST_ROUTES === 'true') {
         route('/__tests/login', '__test_routes__/login.tsx')
       }
     })
