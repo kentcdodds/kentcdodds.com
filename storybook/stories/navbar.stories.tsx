1e508972e7f8b863e7b9fc1534850349e4333ee3
Kent C. Dodds <me+github@kentcdodds.com>
2021-06-28 14:34:35 -0600

    remove integrated stories

--- storybook/stories/navbar.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import {Navbar} from '@kcd/components/navbar'
-
-export default {
-  title: 'Navbar',
-  component: Navbar,
-}
-
-export const StandardNavbar = () => <Navbar />
--- storybook/stories/pages.stories.tsx
+++
@@ -1,8 +0,0 @@
-import {HomePage} from '@kcd/components/pages/home'
-import type {Meta} from '@storybook/react'
-
-export default {
-  title: 'Pages',
-} as Meta
-
-export {HomePage}
