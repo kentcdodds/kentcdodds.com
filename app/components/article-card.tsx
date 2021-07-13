ecb0f7ea1a162ee5d868352e96656d5ecf424b82
Stephan Meijer <stephan.meijer@gmail.com>
2021-07-13 15:06:53 +0200

    add focus-ring to article cards

--- app/components/article-card.tsx
+++ app/components/article-card.tsx
@@ -17,29 +17,34 @@ function ArticleCard({
   },
 }: MdxListItem) {
   return (
-    <a className="group relative w-full" href={`/blog/${slug}`}>
-      <button className="absolute z-10 left-6 top-6 p-4 text-black whitespace-nowrap text-lg font-medium bg-white rounded-lg group-hover:opacity-100 transition lg:px-8 lg:py-4 lg:opacity-0">
+    <div className="relative w-full">
+      <a
+        className="group peer relative block w-full focus:outline-none"
+        href={`/blog/${slug}`}
+      >
+        <div className="aspect-w-3 aspect-h-4 w-full rounded-lg transition group-hover:ring-2 group-focus:ring-2 ring-team-current ring-offset-4 dark:ring-offset-gray-900 ring-offset-white">
+          <img
+            alt={bannerAlt}
+            className="rounded-lg object-cover"
+            src={bannerUrl}
+          />
+        </div>
+
+        <div className="mt-8 text-blueGray-500 text-xl font-medium">
+          {formatDate(parseISO(date), 'PPP')} — {readTime?.text ?? 'quick read'}
+        </div>
+        <div className="mt-4">
+          <H3>{title}</H3>
+        </div>
+      </a>
+
+      <button className="absolute left-6 top-6 p-4 text-black whitespace-nowrap text-lg font-medium hover:bg-gray-200 focus:bg-gray-200 bg-white rounded-lg focus:outline-none shadow hover:shadow-lg focus:shadow-lg peer-hover:opacity-100 hover:opacity-100 peer-focus:opacity-100 focus:opacity-100 transition lg:px-8 lg:py-4 lg:opacity-0">
         <span className="hidden lg:inline">Click to copy url</span>
         <span className="inline lg:hidden">
           <CopyIcon />
         </span>
       </button>
-
-      <div className="aspect-w-3 aspect-h-4 w-full">
-        <img
-          alt={bannerAlt}
-          className="rounded-lg object-cover"
-          src={bannerUrl}
-        />
-      </div>
-
-      <div className="mt-8 text-blueGray-500 text-xl font-medium">
-        {formatDate(parseISO(date), 'PPP')} — {readTime?.text ?? 'quick read'}
-      </div>
-      <div className="mt-4 group-hover:underline">
-        <H3>{title}</H3>
-      </div>
-    </a>
+    </div>
   )
 }
 
