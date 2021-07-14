7f59be2e9bfc9b26ea68b3c4aeae01b458c39aa9
Stephan Meijer <stephan.meijer@gmail.com>
2021-07-14 16:11:12 +0200

    fix: share classname between Button and ButtonLink

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -7,23 +7,32 @@ interface ButtonProps {
   children: React.ReactNode | React.ReactNode[]
 }
 
+function getClassName({
+  variant,
+  className,
+}: {
+  variant: ButtonProps['variant']
+  className?: string
+}) {
+  return clsx(
+    'focus-ring inline-flex items-center justify-center px-11 py-6 whitespace-nowrap text-lg font-medium rounded-full opacity-100 disabled:opacity-50 space-x-5 transition',
+    {
+      'border-2 dark:border-gray-600 border-gray-200 text-primary bg-primary':
+        variant === 'secondary',
+      'bg-inverse text-inverse': variant === 'primary',
+    },
+    className,
+  )
+}
+
 function Button({
   children,
   variant = 'primary',
+  className,
   ...buttonProps
 }: ButtonProps & JSX.IntrinsicElements['button']) {
   return (
-    <button
-      {...buttonProps}
-      className={clsx(
-        'focus-ring inline-flex items-center justify-center px-11 py-6 whitespace-nowrap text-lg font-medium rounded-full opacity-100 disabled:opacity-50 space-x-5 transition',
-        {
-          'border-2 dark:border-gray-600 border-gray-200 text-primary bg-primary':
-            variant === 'secondary',
-          'bg-inverse text-inverse': variant === 'primary',
-        },
-      )}
-    >
+    <button {...buttonProps} className={getClassName({variant, className})}>
       {children}
     </button>
   )
@@ -36,18 +45,7 @@ function ButtonLink({
   to,
 }: ButtonProps & Pick<LinkProps, 'to' | 'className'>) {
   return (
-    <Link
-      to={to}
-      className={clsx(
-        'focus-ring inline-flex items-center justify-center px-11 py-6 whitespace-nowrap text-lg font-medium rounded-full opacity-100 disabled:opacity-50 space-x-5 transition',
-        {
-          'border-2 dark:border-gray-600 border-gray-200 text-primary bg-primary':
-            variant === 'secondary',
-          'bg-inverse text-inverse': variant === 'primary',
-        },
-        className,
-      )}
-    >
+    <Link to={to} className={getClassName({variant, className})}>
       {children}
     </Link>
   )
