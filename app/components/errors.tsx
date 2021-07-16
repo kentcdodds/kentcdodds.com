0236c198ba903ba5c02554c2152f28decb019a21
Stephan Meijer <stephan.meijer@gmail.com>
2021-07-16 16:53:00 +0200

    feat: add redbox for server errors in dev env

--- app/components/errors.tsx
+++ app/components/errors.tsx
@@ -4,10 +4,66 @@ import {images} from '../images'
 import {articles} from '../utils/temp.fixtures'
 import {HeroSection} from './sections/hero-section'
 import {BlogSection} from './sections/blog-section'
+import {H2, H6} from './typography'
+import errorStack from 'error-stack-parser'
+import clsx from 'clsx'
 
-function ErrorPage({title, subtitle}: {title: string; subtitle: string}) {
+function RedBox({error}: {error: Error}) {
+  const [isVisible, setIsVisible] = React.useState(true)
+  const frames = errorStack.parse(error)
+
   return (
-    <main>
+    <div
+      className={clsx(
+        'fixed z-10 inset-0 flex items-center justify-center transition',
+        {
+          'opacity-0 pointer-events-none': !isVisible,
+        },
+      )}
+    >
+      <button
+        className="absolute inset-0 block w-full h-full bg-black opacity-75"
+        onClick={() => setIsVisible(false)}
+      />
+      <div className="border-lg text-primary relative mx-5vw my-16 p-12 max-h-75vh bg-red-500 rounded-lg overflow-y-auto">
+        <H2>{error.message}</H2>
+        <div>
+          {frames.map(frame => (
+            <div
+              key={[frame.fileName, frame.lineNumber, frame.columnNumber].join(
+                '-',
+              )}
+              className="pt-4"
+            >
+              <H6 as="div" className="pt-2">
+                {frame.functionName}
+              </H6>
+              <div className="font-mono opacity-75">
+                {frame.fileName}:{frame.lineNumber}:{frame.columnNumber}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function ErrorPage({
+  title,
+  subtitle,
+  error,
+}: {
+  title: string
+  subtitle: string
+  error?: Error
+}) {
+  return (
+    <main className="relative">
+      {error && process.env.NODE_ENV === 'development' ? (
+        <RedBox error={error} />
+      ) : null}
+
       <HeroSection
         title={title}
         subtitle={subtitle}
@@ -41,7 +97,7 @@ function FourOhFour() {
   )
 }
 
-function ServerError() {
+function ServerError({error}: {error?: Error}) {
   const matches = useMatches()
   const last = matches[matches.length - 1]
   const pathname = last?.pathname
@@ -50,6 +106,7 @@ function ServerError() {
     <ErrorPage
       title="500 - Oh no, something did not go that well."
       subtitle={`"${pathname}" is currently not working. So sorry.`}
+      error={error}
     />
   )
 }
