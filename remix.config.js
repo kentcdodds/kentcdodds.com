7811e95c14584e1aded07335b4edd90f89199db7
Kent C. Dodds <me+github@kentcdodds.com>
2021-07-19 17:15:09 -0600

    improve ensurance that test routes will NOT be enabled on fly

--- remix.config.js
+++ remix.config.js
@@ -4,9 +4,15 @@ module.exports = {
     return defineRoutes(route => {
       if (process.env.ENABLE_TEST_ROUTES === 'true') {
         if (process.env.NODE_ENV === 'production') {
-          console.log(
-            '⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ TEST ROUTES ARE ENABLED and NODE_ENV is production. Make sure this is ok. ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️',
+          console.warn(
+            '⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ENABLE_TEST_ROUTES is true and NODE_ENV is "production". Make sure this is ok. ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️',
           )
+        }
+        if (process.env.FLY_REGION) {
+          console.error(
+            `🚨 🚨 🚨 🚨 ENABLE_TEST_ROUTES is true and FLY_REGION is ${process.env.FLY_REGION} so we're not going to enable test routes because this is probably a mistake. We do NOT want test routes enabled on Fly. 🚨 🚨 🚨 🚨 🚨`,
+          )
+          return
         }
         route('/__tests/login', '__test_routes__/login.tsx')
       }
