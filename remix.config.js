690924a3638cd0297458e9891fb1a64dff7704eb
Kent C. Dodds <me+github@kentcdodds.com>
2021-07-20 08:43:19 -0600

    fix warning about test routes

--- remix.config.js
+++ remix.config.js
@@ -3,14 +3,9 @@ module.exports = {
   routes(defineRoutes) {
     return defineRoutes(route => {
       if (process.env.ENABLE_TEST_ROUTES === 'true') {
-        if (process.env.NODE_ENV === 'production') {
+        if (process.env.NODE_ENV === 'production' && process.env.FLY_APP_NAME) {
           console.warn(
-            '⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ENABLE_TEST_ROUTES is true and NODE_ENV is "production". Make sure this is ok. ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️',
-          )
-        }
-        if (process.env.FLY_REGION) {
-          console.error(
-            `🚨 🚨 🚨 🚨 ENABLE_TEST_ROUTES is true and FLY_REGION is ${process.env.FLY_REGION} so we're not going to enable test routes because this is probably a mistake. We do NOT want test routes enabled on Fly. 🚨 🚨 🚨 🚨 🚨`,
+            `🚨 🚨 🚨 🚨 ENABLE_TEST_ROUTES is true, NODE_ENV is "production" and FLY_APP_NAME is ${process.env.FLY_APP_NAME} so we're not going to enable test routes because this is probably a mistake. We do NOT want test routes enabled on Fly. 🚨 🚨 🚨 🚨 🚨`,
           )
           return
         }
