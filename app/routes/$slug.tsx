3ce74875b35f2a069198cffeeeac9203f4d46537
Stephan Meijer <stephan.meijer@gmail.com>
2021-07-21 17:11:51 +0200

    feat: render content/pages using prose classes

--- app/routes/$slug.tsx
+++ app/routes/$slug.tsx
@@ -9,6 +9,9 @@ import {
 } from '../utils/mdx'
 import {FourOhFour} from '../components/errors'
 import {getUser} from '../utils/session.server'
+import {Grid} from '../components/grid'
+import {BackLink} from '../components/arrow-button'
+import {H2, H6} from '../components/typography'
 
 export const loader: KCDLoader<{slug: string}> = async ({params, request}) => {
   const pageMeta = {
@@ -42,13 +45,33 @@ function MdxScreen({mdxPage}: {mdxPage: MdxPage}) {
 
   return (
     <>
-      <header>
-        <h2>{frontmatter.title}</h2>
-        <p>{frontmatter.description}</p>
-      </header>
-      <main>
+      <Grid className="mb-10 mt-24 lg:mb-24">
+        <div className="flex col-span-full justify-between lg:col-span-8 lg:col-start-3">
+          <BackLink to="/">Back to home</BackLink>
+        </div>
+      </Grid>
+
+      <Grid className="mb-12">
+        <div className="col-span-full lg:col-span-8 lg:col-start-3">
+          <H2>{frontmatter.title}</H2>
+          {frontmatter.description ? (
+            <H6 as="p" variant="secondary" className="mt-2 lowercase">
+              {frontmatter.description}
+            </H6>
+          ) : null}
+        </div>
+        {frontmatter.bannerUrl ? (
+          <img
+            className="col-span-full mt-10 mx-auto rounded-lg lg:col-span-10 lg:col-start-2"
+            src={frontmatter.bannerUrl}
+            alt={frontmatter.bannerAlt}
+          />
+        ) : null}
+      </Grid>
+
+      <Grid className="prose prose-light dark:prose-dark">
         <Component />
-      </main>
+      </Grid>
     </>
   )
 }
