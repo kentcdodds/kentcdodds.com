e092a9ed9f03078f8db442399722bae84ce0a9ca
Kent C. Dodds <me+github@kentcdodds.com>
2021-07-29 17:02:19 -0600

    Don't even bother loading fathom in dev

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -6,10 +6,12 @@ import {RemixBrowser as Remix} from 'remix'
 
 ReactDOM.hydrate(<Remix />, document)
 
-load('HJUUDKMT', {
-  // TODO: update this to the kentcdodds.com domain
-  // url: 'https://sailfish.kentcdodds.com/script.js',
-  url: 'https://aardvark.kent.dev/script.js',
-  spa: 'history',
-  excludedDomains: ['localhost'],
-})
+if (ENV.NODE_ENV !== 'development') {
+  load('HJUUDKMT', {
+    // TODO: update this to the kentcdodds.com domain
+    // url: 'https://sailfish.kentcdodds.com/script.js',
+    url: 'https://aardvark.kent.dev/script.js',
+    spa: 'history',
+    excludedDomains: ['localhost'],
+  })
+}
