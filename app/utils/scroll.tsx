cef754039e76266c027f0dcd0bc34261ca3a6bc7
Kent C. Dodds <me+github@kentcdodds.com>
2021-08-09 16:09:48 -0600

    fix scroll

--- app/utils/scroll.tsx
+++ app/utils/scroll.tsx
@@ -1,11 +1,11 @@
-import {useEffect, useLayoutEffect, useRef} from 'react'
+import * as React from 'react'
 import {useLocation} from 'react-router-dom'
 import {useTransition} from '@remix-run/react'
 
 let firstRender = true
 
 const useSSRLayoutEffect =
-  typeof window === 'undefined' ? () => {} : useLayoutEffect
+  typeof window === 'undefined' ? () => {} : React.useLayoutEffect
 
 if (
   typeof window !== 'undefined' &&
@@ -14,20 +14,26 @@ if (
   window.history.scrollRestoration = 'manual'
 }
 
+// shouldn't have to do it this way
+// https://github.com/remix-run/remix/issues/240
+type LocationState = undefined | {isSubmission: boolean}
 export function useScrollRestoration(enabled: boolean = true) {
-  const positions = useRef<Map<string, number>>(new Map()).current
+  const positions = React.useRef<Map<string, number>>(new Map()).current
   const location = useLocation()
+  const isSubmission = (location.state as LocationState)?.isSubmission ?? false
   const transition = useTransition()
 
-  useEffect(() => {
+  React.useEffect(() => {
+    if (isSubmission) return
     if (transition.state === 'loading') {
       positions.set(location.key, window.scrollY)
     }
-  }, [transition.state, location, positions])
+  }, [transition.state, location.key, positions, isSubmission])
 
   useSSRLayoutEffect(() => {
     if (!enabled) return
     if (transition.state !== 'idle') return
+    if (isSubmission) return
     // don't restore scroll on initial render
     if (firstRender) {
       firstRender = false
@@ -35,23 +41,23 @@ export function useScrollRestoration(enabled: boolean = true) {
     }
     const y = positions.get(location.key)
     window.scrollTo(0, y ?? 0)
-  }, [transition.state, location, positions])
+  }, [transition.state, location.key, positions, isSubmission])
 }
 
 export function useElementScrollRestoration(
   ref: React.MutableRefObject<HTMLElement | null>,
   enabled: boolean = true,
 ) {
-  const positions = useRef<Map<string, number>>(new Map()).current
+  const positions = React.useRef<Map<string, number>>(new Map()).current
   const location = useLocation()
   const transition = useTransition()
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (!ref.current) return
     if (transition.state === 'loading') {
       positions.set(location.key, ref.current.scrollTop)
     }
-  }, [transition.state, location, ref, positions])
+  }, [transition.state, location.key, ref, positions])
 
   useSSRLayoutEffect(() => {
     if (!enabled) return
@@ -59,5 +65,5 @@ export function useElementScrollRestoration(
     if (!ref.current) return
     const y = positions.get(location.key)
     ref.current.scrollTo(0, y ?? 0)
-  }, [transition.state, location, positions, ref])
+  }, [transition.state, location.key, positions, ref])
 }
