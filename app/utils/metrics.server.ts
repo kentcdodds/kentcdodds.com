151e98cc96bc24b2d712d99cb7b5fd18651e65aa
Kent C. Dodds <me+github@kentcdodds.com>
2021-09-17 13:09:14 -0600

    stop logging server timings

--- app/utils/metrics.server.ts
+++ app/utils/metrics.server.ts
@@ -27,7 +27,6 @@ async function time<ReturnType>({
 }
 
 function getServerTimeHeader(timings: Timings) {
-  console.dir({timings}, {color: true, depth: 6})
   return Object.entries(timings)
     .map(([key, timingInfos]) => {
       const dur = timingInfos
