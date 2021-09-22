f69e087eeb9d02b198d6268202a6fc9810387c01
Andrew Li <mail@andrewli.ca>
2021-09-21 23:25:45 -0700

    Fix testing javascript course link

--- app/components/sections/course-section.tsx
+++ app/components/sections/course-section.tsx
@@ -29,7 +29,7 @@ function CourseSection() {
             title="Testing JavaScript"
             description="Learn smart, efficient testing methods."
             imageBuilder={images.courseTestingJS}
-            courseUrl="https://testing-library.com"
+            courseUrl="https://testingjavascript.com"
           />
         </div>
       </Grid>
