715513ce126c76fb967bedbd7f0d266bdcebfafb
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2021-10-05 22:27:13 +0200

    fix: Center DarkModeToggle icons if browser font size is changed (#150)

--- app/components/icons/moon-icon.tsx
+++ app/components/icons/moon-icon.tsx
@@ -3,8 +3,7 @@ import * as React from 'react'
 function MoonIcon() {
   return (
     <svg
-      width="32"
-      height="32"
+      className="w-full"
       viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
