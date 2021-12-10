3f6637c2010a2821aba5358c84edbb7fd5d34047
Kent C. Dodds <me+github@kentcdodds.com>
2021-12-10 14:14:59 -0700

    remove rsync stuff with new postcss-cli version

--- other/pm2.config.js
+++ other/pm2.config.js
@@ -35,16 +35,9 @@ module.exports = {
       script: 'node ./other/refresh-on-content-changes.js',
       ignore_watch: ['.'],
     },
-
-    // We build postcss to other/postcss.ignored and use rsync to copy them to
-    // the app directory. If we point postcss directly to the app directory then
-    // it updates the files even if there are no changes, which triggers a
-    // double rebuild. So we use rsync with --checksum so it only updates the
-    // files when their contents have changed.
     {
       name: 'Postcss',
-      script:
-        'postcss styles/**/*.css --base styles --dir other/postcss.ignored',
+      script: 'postcss styles/**/*.css --base styles --dir app/styles',
       autorestart: false,
       watch: [
         './tailwind.config.js',
@@ -54,15 +47,6 @@ module.exports = {
       ],
       env: {
         NODE_ENV: process.env.NODE_ENV ?? 'development',
-        FORCE_COLOR: '1',
-      },
-    },
-    {
-      name: 'rsync',
-      script: 'rsync -v --checksum -r other/postcss.ignored/ app/styles',
-      watch: ['other/postcss.ignored'],
-      autorestart: false,
-      env: {
         FORCE_COLOR: '1',
       },
     },
