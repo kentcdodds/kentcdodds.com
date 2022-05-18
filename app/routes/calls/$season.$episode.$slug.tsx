ea8b33b5e9e427cdd28467d5626635e87c8e62b3
Kent C. Dodds <me+github@kentcdodds.com>
2022-05-18 16:59:58 -0600

    return response

--- app/routes/calls/$season.$episode.$slug.tsx
+++ app/routes/calls/$season.$episode.$slug.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
-import type {LoaderFunction, MetaFunction} from '@remix-run/node';
-import { redirect} from '@remix-run/node'
+import type {LoaderFunction, MetaFunction} from '@remix-run/node'
+import {json, redirect} from '@remix-run/node'
 import {useParams} from '@remix-run/react'
 import type {LoaderData as RootLoaderData} from '../../root'
 import type {KCDHandle} from '~/types'
 import {getEpisodes} from '~/utils/transistor.server'
 import {Themed} from '~/utils/theme-provider'
-import type { Params} from '~/utils/call-kent';
+import type {Params} from '~/utils/call-kent'
 import {getEpisodeFromParams, getEpisodePath} from '~/utils/call-kent'
-import type {LoaderData as CallsLoaderData} from '../calls';
-import { useCallsData} from '../calls'
+import type {LoaderData as CallsLoaderData} from '../calls'
+import {useCallsData} from '../calls'
 import {getSocialMetas} from '~/utils/seo'
 import {getUrl} from '~/utils/misc'
 import {H6, Paragraph} from '~/components/typography'
@@ -105,7 +105,7 @@ export const loader: LoaderFunction = async ({params, request}) => {
   // wasteful to send it here. The parent sticks all the episodes in context
   // so we just use it in the component.
   // This loader is only here for the 404 case we need to handle.
-  return null
+  return json({})
 }
 
 export default function Screen() {
