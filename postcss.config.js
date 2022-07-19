43bafde1bdff049620b79454cdd567077d8fa71b
Kent C. Dodds <me+github@kentcdodds.com>
2022-07-19 14:24:12 -0600

    minify css in production

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,10 @@ module.exports = {
     require('tailwindcss'),
     require('autoprefixer'),
     require('postcss-import'),
-  ],
+    process.env.NODE_ENV === 'production'
+      ? require('cssnano')({
+          preset: 'default',
+        })
+      : null,
+  ].filter(Boolean),
 }
