5ae3d6d31608f5b7426a632a1a92aa675253afab
Kent C. Dodds <me+github@kentcdodds.com>
2022-10-12 05:37:53 -0600

    Fix break

--- app/routes/blog.$slug.tsx
+++ app/routes/blog.$slug.tsx
@@ -558,7 +558,7 @@ export default function MdxScreen() {
           ) : null}
         </Grid>
 
-        <Grid className="prose prose-light mb-24 break-all dark:prose-dark">
+        <Grid className="prose prose-light mb-24 break-words dark:prose-dark">
           <Component />
         </Grid>
       </main>
