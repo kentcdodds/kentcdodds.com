6bcef7702989c9e1d824afac9babeceeb742ddc3
Kent C. Dodds <me+github@kentcdodds.com>
2022-10-20 21:13:58 -0600

    Update blog.$slug.tsx

--- app/routes/blog.$slug.tsx
+++ app/routes/blog.$slug.tsx
@@ -374,10 +374,9 @@ function ArticleFooter({
         <H6 as="div">Written by Kent C. Dodds</H6>
         <Paragraph className="mb-12 mt-3">
           {`
-Kent C. Dodds is a JavaScript software engineer and teacher. He's Co-Founder and
-Director of Developer Experience at Remix! Kent's taught hundreds of thousands of
-people how to make the world a better place with quality software development tools
-and practices. He lives with his wife and four kids in Utah.
+Kent C. Dodds is a JavaScript software engineer and teacher. Kent's taught hundreds
+of thousands of people how to make the world a better place with quality software
+development tools and practices. He lives with his wife and four kids in Utah.
           `.trim()}
         </Paragraph>
         <ArrowLink to="/about">Learn more about Kent</ArrowLink>
