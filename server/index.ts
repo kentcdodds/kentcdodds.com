5c0f7966351f4eca548bba9e75e214279f3b0a07
Kent C. Dodds <me+github@kentcdodds.com>
2022-11-24 11:28:11 -0700

    enable metronome

--- server/index.ts
+++ server/index.ts
@@ -129,7 +129,7 @@ app.use((req, res, next) => {
 
 app.use(morgan('tiny'))
 
-const enableMetronome = false
+const enableMetronome = true
 
 function getRequestHandlerOptions(): Parameters<
   typeof createRequestHandler
