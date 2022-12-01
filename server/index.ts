7cc9440e122e5db4a33de39ecba5bf234263ec58
Kent C. Dodds <me+github@kentcdodds.com>
2022-11-30 19:06:01 -0700

    disable report only

--- server/index.ts
+++ server/index.ts
@@ -58,7 +58,6 @@ app.use(
   helmet({
     crossOriginEmbedderPolicy: false,
     contentSecurityPolicy: {
-      reportOnly: true,
       directives: {
         'connect-src': MODE === 'development' ? ['ws:', "'self'"] : null,
         'font-src': ["'self'"],
