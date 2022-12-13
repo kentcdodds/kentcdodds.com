6356573e9bc37116088050902b7634690caea286
Kent C. Dodds <me+github@kentcdodds.com>
2022-12-13 13:06:33 -0700

    dump heapdump

--- app/routes/resources/heapsnapshot.ts
+++ app/routes/resources/heapsnapshot.ts
@@ -1,7 +1,6 @@
 import path from 'path'
-import os from 'os'
 import fs from 'fs'
-import heapdump from 'heapdump'
+import v8 from 'v8'
 import {Response} from '@remix-run/node'
 import {PassThrough} from 'stream'
 import type {DataFunctionArgs} from '@remix-run/node'
@@ -9,23 +8,8 @@ import {requireAdminUser} from '~/utils/session.server'
 
 export async function loader({request}: DataFunctionArgs) {
   await requireAdminUser(request)
-  const host =
-    request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
 
-  const snapshotPath = await new Promise<string | undefined>(
-    (resolve, reject) => {
-      const tempDir = os.tmpdir()
-      const filepath = path.join(
-        tempDir,
-        `${host}-heapdump-${Date.now()}.heapsnapshot`,
-      )
-      console.log(`Writing heapdump to ${filepath}`)
-      heapdump.writeSnapshot(filepath, (err, filename) => {
-        if (err) reject(err)
-        resolve(filename)
-      })
-    },
-  )
+  const snapshotPath = v8.writeHeapSnapshot()
   if (!snapshotPath) {
     throw new Response('No snapshot saved', {status: 500})
   }
