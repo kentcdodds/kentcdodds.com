7e675ae669968e5a559f01161fb77885800cb33e
Kent C. Dodds <me+github@kentcdodds.com>
2023-01-03 11:26:15 -0700

    get rid of snowfall

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -22,7 +22,7 @@ import {useTeam} from '~/utils/team-provider'
 import {Theme, Themed, useTheme} from '~/utils/theme-provider'
 import {useOptionalUser, useRootData} from '~/utils/use-root-data'
 import {useElementState} from './hooks/use-element-state'
-import {MoonIcon, SnowflakeIcon, SunIcon} from './icons'
+import {MoonIcon, SunIcon} from './icons'
 import {TeamCircle} from './team-circle'
 
 const LINKS = [
@@ -318,53 +318,6 @@ function ProfileButton({
   )
 }
 
-// https://embed.im/snow/
-function letItSnow() {
-  let embedimSnow = document.getElementById('embedim--snow')
-  function embRand(a: number, b: number) {
-    return Math.floor(Math.random() * (b - a + 1)) + a
-  }
-  if (embedimSnow) {
-    embedimSnow.remove()
-    return
-  }
-
-  let embCSS =
-    '.embedim-snow{position: absolute;width: 10px;height: 10px;background: white;border-radius: 50%;margin-top:-10px}.light .embedim-snow{background: #d1edff}'
-  let embHTML = ''
-  for (let i = 1; i < 200; i++) {
-    embHTML += '<i class="embedim-snow"></i>'
-    const rndX = embRand(0, 1000000) * 0.0001
-    const rndO = embRand(-100000, 100000) * 0.0001
-    const rndT = (embRand(3, 8) * 10).toFixed(2)
-    const rndS = (embRand(0, 10000) * 0.0001).toFixed(2)
-    embCSS +=
-      `.embedim-snow:nth-child(${i}){` +
-      `opacity:${(embRand(1, 10000) * 0.0001).toFixed(2)};` +
-      `transform:translate(${rndX.toFixed(2)}vw,-10px) scale(${rndS});` +
-      `animation:fall-${i} ${embRand(10, 30)}s -${embRand(
-        0,
-        30,
-      )}s linear infinite` +
-      `}` +
-      `@keyframes fall-${i}{${rndT}%{` +
-      `transform:translate(${(rndX + rndO).toFixed(
-        2,
-      )}vw,${rndT}vh) scale(${rndS})` +
-      `}` +
-      `to{` +
-      `transform:translate(${(rndX + rndO / 2).toFixed(
-        2,
-      )}vw, 105vh) scale(${rndS})` +
-      `}` +
-      `}`
-  }
-  embedimSnow = document.createElement('div')
-  embedimSnow.id = 'embedim--snow'
-  embedimSnow.innerHTML = `<style>#embedim--snow{position:fixed;left:0;top:0;bottom:0;width:100vw;height:100vh;overflow:hidden;z-index:9999999;pointer-events:none}${embCSS}</style>${embHTML}`
-  document.body.appendChild(embedimSnow)
-}
-
 function Navbar() {
   const [team] = useTeam()
   const {requestInfo, userInfo} = useRootData()
@@ -374,9 +327,6 @@ function Navbar() {
     <div className="px-5vw py-9 lg:py-12">
       <nav className="text-primary mx-auto flex max-w-8xl items-center justify-between">
         <div className="flex justify-center gap-4 align-middle">
-          <button onClick={letItSnow} className="animate-spin-xslow">
-            <SnowflakeIcon title="Let it snow" />
-          </button>
           <Link
             prefetch="intent"
             to="/"
