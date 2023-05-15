1863b0a7b7fa2c3c4c04262faef722e907cbb15b
Kent C. Dodds <me+github@kentcdodds.com>
2023-05-15 16:19:07 -0600

    add some logging to cache.sqlite route

--- app/routes/resources/cache.sqlite.ts
+++ app/routes/resources/cache.sqlite.ts
@@ -15,13 +15,18 @@ export async function action({request}: DataFunctionArgs) {
   const isAuthorized =
     request.headers.get('Authorization') === `Bearer ${token}`
   if (!isAuthorized) {
+    console.log(
+      `Unauthorized request to ${request.url}, redirecting to solid tunes 🎶`,
+    )
     // rick roll them
     return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
   }
   const {key, cacheValue} = await request.json()
   if (cacheValue === undefined) {
+    console.log(`Deleting ${key} from the cache from remote`)
     await cache.delete(key)
   } else {
+    console.log(`Setting ${key} in the cache from remote`)
     await cache.set(key, cacheValue)
   }
   return json({success: true})
