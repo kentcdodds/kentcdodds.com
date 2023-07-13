830927510dc71633a7b2288887ce708188809923
Amir Ziaei <26747997+amir-ziaei@users.noreply.github.com>
2023-07-13 23:05:29 +0300

    fix: broken meta on single call's  episode page (#468)
    Co-authored-by: Amir Ziaei <amir.ziaei@zenitech.co.uk>

--- app/routes/calls+/$season.$episode_.$slug.tsx
+++ app/routes/calls+/$season.$episode_.$slug.tsx
@@ -48,11 +48,11 @@ export const handle: KCDHandle = {
 
 export const meta: V2_MetaFunction<
   typeof loader,
-  {root: RootLoaderType; 'routes/calls+/_calls': typeof callsLoader}
+  {root: RootLoaderType; 'routes/calls': typeof callsLoader}
 > = ({matches, params}) => {
   const {requestInfo} = matches.find(m => m.id === 'root')
     ?.data as RootLoaderData
-  const callsData = matches.find(m => m.id === 'routes/calls+/_calls')?.data as
+  const callsData = matches.find(m => m.id === 'routes/calls')?.data as
     | CallsLoaderData
     | undefined
   if (!callsData) {
