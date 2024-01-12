8024dd7777f3210962972dc66001e689ea74f031
Pedro Cattori <pcattori@gmail.com>
2024-01-12 15:33:07 -0500

    cjs interop

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import {defineConfig} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import {flatRoutes} from 'remix-flat-routes'
 import envOnly from 'vite-env-only'
+import {cjsInterop} from 'vite-plugin-cjs-interop'
 
 export default defineConfig(() => {
   return {
     plugins: [
+      cjsInterop({
+        dependencies: [
+          '@remark-embedder/core',
+          '@remark-embedder/transformer-oembed',
+        ],
+      }),
       envOnly(),
       remix({
         ignoredRouteFiles: ['**/*'],
