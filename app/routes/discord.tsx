e17c2f42e11012ff03eed75bc1343ef049aa15c6
Kent C. Dodds <me+github@kentcdodds.com>
2024-03-20 11:00:51 -0600

    fix lint

--- app/routes/discord.tsx
+++ app/routes/discord.tsx
@@ -13,7 +13,6 @@ import {
 } from '@remix-run/node'
 import {Outlet, useLoaderData} from '@remix-run/react'
 import {motion} from 'framer-motion'
-import {ArrowLink} from '~/components/arrow-button.tsx'
 import {ButtonLink} from '~/components/button.tsx'
 import {FeatureCard} from '~/components/feature-card.tsx'
 import {Grid} from '~/components/grid.tsx'
@@ -29,7 +28,6 @@ import {
   TrophyIcon,
   UsersIcon,
 } from '~/components/icons.tsx'
-import {NumberedPanel} from '~/components/numbered-panel.tsx'
 import {CourseSection} from '~/components/sections/course-section.tsx'
 import {HeaderSection} from '~/components/sections/header-section.tsx'
 import {HeroSection} from '~/components/sections/hero-section.tsx'
