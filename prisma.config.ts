4a2995c65555caa342789009dc6d070f5c55d8aa
Kent C. Dodds <me+github@kentcdodds.com>
2025-12-06 10:08:03 -0700

    fallback database url

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { defineConfig, env } from 'prisma/config'
+import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
 	schema: 'prisma/schema.prisma',
@@ -8,6 +8,6 @@ export default defineConfig({
 		seed: 'tsx other/runfile prisma/seed.ts',
 	},
 	datasource: {
-		url: env('DATABASE_URL'),
+		url: process.env.DATABASE_URL || 'file:./prisma/sqlite.db',
 	},
 })
